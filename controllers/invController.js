15a685bbc44431b6f91a2512a2bc588df31f6139
SARIAH TANNER <sariahtanner@gmail.com>
2024-10-05 06:23:38 -0600

    still working on it

--- controllers/invController.js
+++ controllers/invController.js
@@ -64,4 +64,81 @@ invCont.addInventory = async function (req, res, next) {
   })
 }
 
+
+//Add Vehicle to Database
+invCont.addVehicle = async function (req, res) {
+  let nav = await utilities.getNav()
+  const {
+    classification_id,
+    inv_make,
+    inv_model,
+    inv_description,
+    inv_image,
+    inv_thumbnail,
+    inv_price,
+    inv_year,
+    inv_miles,
+    inv_color
+  } = req.body
+  console.log(inv_price);
+  const result = await invModel.addVehicle(
+    classification_id,
+    inv_make,
+    inv_model,
+    inv_description,
+    inv_image,
+    inv_thumbnail,
+    inv_price,
+    inv_year,
+    inv_miles,
+    inv_color)
+
+  if (result) {
+    req.flash(
+      "notice",
+      `Congratulations, you\'ve added ${inv_make}, ${inv_model}.`
+    )
+    res.status(201).render("inventory/add-inventory", {
+      title: "Inventory",
+      nav,
+    })
+  } else {
+    req.flash("notice", "Sorry, the inventory addition failed.")
+    res.status(501).render("inventory/add-inventory", {
+      title: "Inventory",
+      nav,
+    })
+  }
+}
+
+//Deliver Add Classification Notice
+invCont.classification = async function (req, res) {
+  let nav = await utilities.getNav()
+  const {
+    classification_name
+  } = req.body
+
+  const result = await invModel.classification(
+    classification_name
+  )
+
+  if (result) {
+    req.flash(
+      "notice",
+      `Congratulations, you\'ve added ${classification_name}.`
+    )
+    res.status(201).render("inventory/add-classification", {
+      title: "classification",
+      nav,
+    })
+  } else {
+    req.flash("notice", "Sorry, the classification addition failed.")
+    res.status(501).render("inventory/add-classification", {
+      title: "classification",
+      nav,
+    })
+  }
+}
+
+
 module.exports = invCont
